2f451667d8a30feaf96d3708fb12a6c5b741efb9
Youxuan Zhao <youxuanzhao2007@outlook.com>
2023-09-30 12:33:39 +0800

    resize

--- Scenes/Select.gd
+++ Scenes/Select.gd
@@ -1,7 +1,19 @@
 extends Sprite2D
 
+@export var map : TileMap
+var windowSize : Vector2i
+
 func _ready():
 	$whitecover.visible = false
+	windowSize = DisplayServer.screen_get_size()
+	get_viewport().set_content_scale_size(Vector2i(80,80))
+	DisplayServer.window_set_size(Vector2i(windowSize.y/4, windowSize.y/4))
+	
+	
 func _input(event):
 	if event.is_action_pressed("space"):
 		$whitecover.visible = !($whitecover.visible)
+	if event.is_action_pressed("test"):
+		DisplayServer.window_set_size(Vector2i(windowSize.y/2, windowSize.y/4))
+		get_viewport().set_content_scale_size(Vector2i(160,80))
+		
