8f4ce5b1c80258b7bd5b6bb534300925104e64bb
Youxuan Zhao <youxuanzhao2007@outlook.com>
2023-09-30 17:31:37 +0800

    walkinganimation

--- Scenes/Select.gd
+++ Scenes/Select.gd
@@ -1,19 +1,13 @@
 extends Sprite2D
 
 @export var map : TileMap
-var windowSize : Vector2i
 
 func _ready():
+	$AnimationPlayer.play("idle")
 	$whitecover.visible = false
-	windowSize = DisplayServer.screen_get_size()
-	get_viewport().set_content_scale_size(Vector2i(80,80))
-	DisplayServer.window_set_size(Vector2i(windowSize.y/4, windowSize.y/4))
 	
 	
 func _input(event):
 	if event.is_action_pressed("space"):
 		$whitecover.visible = !($whitecover.visible)
-	if event.is_action_pressed("test"):
-		DisplayServer.window_set_size(Vector2i(windowSize.y/2, windowSize.y/4))
-		get_viewport().set_content_scale_size(Vector2i(160,80))
-		
+	
--- Scenes/player.gd
+++ Scenes/player.gd
@@ -2,7 +2,7 @@ extends CharacterBody2D
 
 
 func _ready():
-	var selection = $Selected
+	pass
 	
 func _input(event):
 	
@@ -10,24 +10,29 @@ func _input(event):
 	
 	if event.is_action_pressed("move_right"):
 		velocity.x = 8
+		
 	elif event.is_action_pressed("move_left"):
 		velocity.x = -8
+		
 	elif event.is_action_pressed("move_up"):
 		velocity.y = -8
+		
 	elif event.is_action_pressed("move_down"):
 		velocity.y = 8
 		
+		
 	if event.is_action_pressed("arrow_right"):
 		$Selected.position = Vector2(8,0)
-
+		$Sprite2D.frame = 1
 	elif event.is_action_pressed("arrow_left"):
 		$Selected.position = Vector2(-8,0)
-
+		$Sprite2D.frame = 0
 	elif event.is_action_pressed("arrow_up"):
 		$Selected.position = Vector2(0,-8)
-
+		$Sprite2D.frame = 2
 	elif event.is_action_pressed("arrow_down"):
 		$Selected.position = Vector2(0,8)
+		$Sprite2D.frame = 3
 	
 	var coll = move_and_collide(velocity)
 	if coll:
