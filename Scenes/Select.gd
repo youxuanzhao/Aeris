fee338e6a17d69d178ecaab480e2f5ae67b41f9f
Youxuan Zhao <youxuanzhao2007@outlook.com>
2023-09-30 21:22:38 +0800

    replace block

--- Scenes/Select.gd
+++ Scenes/Select.gd
@@ -2,6 +2,10 @@ extends Sprite2D
 
 @export var map : TileMap
 
+var temp
+
+var stamp_coords
+
 func _ready():
 	$AnimationPlayer.play("idle")
 	$whitecover.visible = false
@@ -9,5 +13,13 @@ func _ready():
 	
 func _input(event):
 	if event.is_action_pressed("space"):
-		$whitecover.visible = !($whitecover.visible)
+		temp = map.get_cell_tile_data(2,map.local_to_map(global_position))
+		print(global_position)
+		if temp != null:
+			map.set_cell(2,map.local_to_map(global_position),-1,Vector2i(-1,-1),-1)
+			map.set_cell(2,stamp_coords,2,Vector2i(0,0),0)
+			$whitecover.visible = false
+		else:
+			stamp_coords = map.local_to_map(global_position)
+			$whitecover.visible = true
 	
