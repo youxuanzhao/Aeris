ae718a371ee18d95384b95a57996e2501cfe18f4
Youxuan Zhao <youxuanzhao2007@outlook.com>
2023-09-30 21:34:55 +0800

    bug fixes

--- Scenes/Select.gd
+++ Scenes/Select.gd
@@ -3,10 +3,11 @@ extends Sprite2D
 @export var map : TileMap
 
 var temp
-
+@onready var can_place : bool = false
 var stamp_coords
 
 func _ready():
+	
 	$AnimationPlayer.play("idle")
 	$whitecover.visible = false
 	
@@ -16,10 +17,16 @@ func _input(event):
 		temp = map.get_cell_tile_data(2,map.local_to_map(global_position))
 		print(global_position)
 		if temp != null:
-			map.set_cell(2,map.local_to_map(global_position),-1,Vector2i(-1,-1),-1)
-			map.set_cell(2,stamp_coords,2,Vector2i(0,0),0)
-			$whitecover.visible = false
+			if can_place:
+				map.set_cell(2,map.local_to_map(global_position),-1,Vector2i(-1,-1),-1)
+				can_place = false
+				$whitecover.visible = false
+			else:
+				$AnimationPlayer.play("warning")
+				$AnimationPlayer.queue("idle")
 		else:
+			can_place = true
 			stamp_coords = map.local_to_map(global_position)
+			map.set_cell(2,stamp_coords,2,Vector2i(0,0),0)
 			$whitecover.visible = true
 	
