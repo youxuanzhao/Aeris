ad97f37d2351afa643f36d8c113e1f44528db608
lihe07 <li@imlihe.com>
2023-10-01 15:24:34 +0800

    menu

---
+++ Scripts/MenuButton.gd
@@ -0,0 +1,20 @@
+extends Button
+
+func set_children_mouse_filter(p: Panel, m: int):
+	for c in p.get_children():
+		if "mouse_filter" in c:
+			c.mouse_filter = m
+
+func _pressed():
+	var anim = $"../AnimationPlayer" as AnimationPlayer
+	var panel = $"../Panel" as Panel
+	if panel.mouse_filter == MOUSE_FILTER_IGNORE:
+		anim.play("fade_in")
+		await anim.animation_finished
+		panel.mouse_filter = MOUSE_FILTER_STOP
+		set_children_mouse_filter(panel, MOUSE_FILTER_STOP)
+	else:
+		anim.play_backwards("fade_in")
+		await anim.animation_finished
+		panel.mouse_filter = MOUSE_FILTER_IGNORE
+		set_children_mouse_filter(panel, MOUSE_FILTER_IGNORE)
