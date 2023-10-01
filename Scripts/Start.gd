6633f607688e3a618e7ffdfc60eac8a5f5ed3c4a
lihe07 <li@imlihe.com>
2023-10-01 14:41:59 +0800

    bugfix

--- Scripts/Start.gd
+++ Scripts/Start.gd
@@ -1,10 +1,12 @@
 extends Button
 
 func _pressed():
-	var anim = $"../../AnimationPlayer" as AnimationPlayer
-	var root = $"../.."
+	var anim = $"../../../AnimationPlayer" as AnimationPlayer
+	var root = $"../../.."
+	var splash = $"../.." as CanvasLayer
 	anim.play("fade_in")
 	await anim.animation_finished
+	splash.visible = false
 	var main = load("res://Scenes/main.tscn").instantiate()
 	root.add_child(main)
 	anim.play_backwards("fade_in")
