054ff9019811160723e6b69962c7fb3e754e18b5
lihe07 <li@imlihe.com>
2023-10-03 02:10:05 +0800

    final

--- Scenes/BGM.gd
+++ Scenes/BGM.gd
@@ -1,3 +1,7 @@
 extends AudioStreamPlayer
 
+func _ready():
+    finished.connect(on_finished)
 
+func on_finished():
+    play(0)
--- Scenes/start.gd
+++ Scenes/start.gd
@@ -6,5 +6,7 @@ class_name StartScreen
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	get_window().size = Vector2i(DisplayServer.screen_get_size().y/4.0,DisplayServer.screen_get_size().y/4.0)
+	$"Splash/TextureRect"
 	var anim = $AnimationPlayer
 	anim.play_backwards("fade_in")
+	
